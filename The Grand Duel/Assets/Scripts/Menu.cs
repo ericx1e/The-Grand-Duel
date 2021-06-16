6704f68a9d72d50c1986cf5db084d9ecb4e5ad94
ericx1e <ericxie6@gmail.com>
2021-06-16 01:06:48 -0400

    multiplayer menu with PUN

---
+++ The Grand Duel/Assets/Scripts/Menu.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+
+public class Menu : MonoBehaviourPunCallbacks
+{
+    [Header("Screens")]
+    public GameObject mainScreen;
+    public GameObject lobbyScreen;
+
+    [Header("Main Screen")]
+    public Button createRoomButton;
+    public Button joinRoomButton;
+    public TMP_InputField roomNameInput;
+    public int roomNameCharacterLimit;
+    public TMP_InputField playerNameInput;
+    public int playerNameCharacterLimit;
+
+    [Header("Lobby Screen")]
+    public TextMeshProUGUI playerListText;
+    public TextMeshProUGUI lobbyInfoText;
+    public Button startGameButton;
+
+    void Start()
+    {
+        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
+        roomNameInput.characterLimit = roomNameCharacterLimit;
+        playerNameInput.characterLimit = playerNameCharacterLimit;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        createRoomButton.interactable = true;
+        joinRoomButton.interactable = true;
+    }
+
+    void SetScreen(GameObject screen)
+    {
+        mainScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+        screen.SetActive(true);
+    }
+
+    public void OnCreateRoomButton()
+    {
+        NetworkManager.instance.CreateRoom(roomNameInput.text);
+    }
+
+    public void OnJoinRoomButton()
+    {
+        NetworkManager.instance.JoinRoom(roomNameInput.text);
+    }
+
+    public void OnPlayerNameUpdate()
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SetScreen(lobbyScreen);
+        lobbyInfoText.SetText("Lobby: " + roomNameInput.text + "\n\n" + "Host: " + PhotonNetwork.MasterClient.NickName);
+
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdateLobbyUI();
+    }
+
+    [PunRPC]
+    public void UpdateLobbyUI()
+    {
+        playerListText.text = "";
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            playerListText.text += player.NickName + "\n";
+        }
+
+        if (PhotonNetwork.IsMasterClient)
+            startGameButton.interactable = true;
+        else
+            startGameButton.interactable = false;
+
+        //lobbyInfoText.text = "Name: " + PhotonNetwork.CurrentLobby.Name + "\n" + "Host: " + PhotonNetwork.MasterClient.NickName;
+    }
+
+    public void OnLeaveLobbyButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        SetScreen(mainScreen);
+    }
+
+    public void OnStartGameButton()
+    {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+    }
+}
---
+++ The Grand Duel/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //public override void OnConnectedToMaster()
+    //{
+    //    Debug.Log("Connected to Master Server");
+    //    CreateRoom("testroom");
+    //    //base.OnConnectedToMaster();
+    //}
+
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    //public override void OnCreatedRoom()
+    //{
+    //    Debug.Log("Created Room: " + PhotonNetwork.CurrentRoom.Name);
+    //    //base.OnCreatedRoom();
+    //}
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
