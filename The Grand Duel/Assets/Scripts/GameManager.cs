a8fbe45fd5309e285ef162747d101b9070882b11
ericx1e <ericxie6@gmail.com>
2021-06-17 01:33:19 -0400

    multiplayer player spawning and orthographic game scene

---
+++ The Grand Duel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public string playerPrefabLocation;
+
+    public static GameManager instance;
+    private int playersInGame = 0;
+
+    public PlayerController[] players;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, new Vector3(0, 10, 0), Quaternion.identity);
+
+        PlayerController playerScipt = playerObj.GetComponent<PlayerController>();
+
+        playerScipt.enabled = true;
+        playerScipt.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+}
---
+++ The Grand Duel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+
+    private Player photonPlayer;
+    private int id;
+    private bool isMine;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+
+        GameManager.instance.players[id - 1] = this;
+        isMine = photonView.IsMine;
+    }
+
+}
